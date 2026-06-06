ef0f8353b26543702d83bc05c331781abd4c3fba
Caio Filipe <caiofelipelg@hotmail.com>
2026-06-06 19:55:38 -0300

    chore: initial project setup with Spring Boot 3.5.14 and Java 21

---
+++ src/main/java/com/aifinancialanalyst/AiFinancialAnalystApplication.java
@@ -0,0 +1,13 @@
+package com.aifinancialanalyst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiFinancialAnalystApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiFinancialAnalystApplication.class, args);
+	}
+
+}
